233284dede420c4f5e3ae54fa2ccf6ccaa720718
wellingtonPLF <wellplf@gmail.com>
2026-03-14 19:42:57 -0300

    Node basic App

---
+++ app.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify();
+
+app.get('/', async (req, rep) => {
+	return 'Nice'
+});
+
+app.get('/hello', async (req, rep) => {
+	return 'HELLO WORLD'
+});
+
+app.listen({port:3333}).then(() => {
+	console.log("SERVER RUNNING on http://localhost:3333")
+});
