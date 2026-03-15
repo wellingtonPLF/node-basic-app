8f5cad23e41902c4ab4d4fa0d91d315b2ab6bcf8
WellingtonPLF <42122392+wellingtonPLF@users.noreply.github.com>
2026-03-14 22:25:48 -0300

    fix host address

--- app.ts
+++ app.ts
@@ -10,6 +10,6 @@ app.get('/hello', async (req, rep) => {
 	return 'HELLO WORLD'
 });
 
-app.listen({port:3333}).then(() => {
+app.listen({port:3333, host: '0.0.0.0'}).then(() => {
 	console.log("SERVER RUNNING on http://localhost:3333")
 });
